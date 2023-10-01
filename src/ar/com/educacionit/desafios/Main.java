21376450e57ccf12ad1b2dae529f0367ae8c9058
Maite Arakaki <maite.arakaki@gmail.com>
2023-10-01 10:27:03 -0300

    ejercicio completo

--- src/ar/com/educacionit/desafios/Main.java
+++ src/ar/com/educacionit/desafios/Main.java
@@ -1,9 +1,21 @@
 package ar.com.educacionit.desafios;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner teclado = new Scanner(System.in);
+		int cantAlumnos = Interaccion.getCantidad(teclado,"alumnos");
+		int cantNotas = Interaccion.getCantidad(teclado,"notas");
+		Alumno[] alumnos = new Alumno[0];
+		
+		alumnos = Interaccion.cargarAlumnos(cantAlumnos, cantNotas, teclado);
+		teclado.close();
+		
+		Interaccion.setPromedioBase(7f);
+		Interaccion.mostrarResultados(alumnos);
+		
 	}
 
 }
